1ae18817130dc262667729b29ee3f031cb91593b
Devbeard <cepoangkuningan@gmail.com>
2024-09-03 07:28:28 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
+// const expressHbs = require('express-handlebars');
+const rootDir = require("./util/path");
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
-    
-server.listen(3000);
+// set pug template handlebars
+// app.engine('hbs',
+//             expressHbs.engine({
+//               layoutsDir:'views/layouts/',
+//               defaultLayout: 'main-layout',
+//               extname: 'hbs'
+//           })
+//         );
+// set pug template engine
+app.set("view engine", "ejs");
+//app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404Page);
+
+app.listen(3000);
