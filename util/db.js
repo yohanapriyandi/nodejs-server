7aa087653d29d64bf8177f37398fcd4cd9c209bd
Devbeard <cepoangkuningan@gmail.com>
2024-11-18 13:46:20 +0700

    change connection string, use mongodb connection

--- util/db.js
+++ util/db.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+// use mongodb for data storage
 
-const sequelize = new Sequelize('node-complete', 'root', 'C1ntaSunn4h234', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongodb = require('mongodb');
 
-module.exports = sequelize;
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+    MongoClient.connect(    
+    'mongodb+srv://yohanapriyandi89:H9oEIGxe8FwKNj1A@cluster0.2mxx4.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+    .then(client => {
+        //console.log('Connected!');
+        _db = client.db();
+        callback()
+    })
+    .catch(err => {
+        console.log(err)
+        throw err;
+    });    
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!'; 
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
