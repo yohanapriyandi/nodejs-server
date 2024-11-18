235eaffa3fdaafe925568f2e8b25503068a2d185
Devbeard <cepoangkuningan@gmail.com>
2024-11-18 13:46:54 +0700

    update routes for shop

--- routes/shop.js
+++ routes/shop.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
-router.get('/product-lists', shopController.getProducts);
+//router.get('/product-lists', shopController.getProducts);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 router.get('/orders', shopController.getOrders);
-// router.get('/checkout', shopController.getCheckout);
+//router.get('/checkout', shopController.getCheckout);
 router.post('/create-order', shopController.postOrder)
 
 module.exports = router;
