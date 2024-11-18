163acb5d95184e33e8a97e25d8ed3ec26aca2511
Devbeard <cepoangkuningan@gmail.com>
2024-11-18 13:47:33 +0700

    update orderjs

--- models/order.js
+++ models/order.js
@@ -1,13 +1,13 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/db');
+// const Sequelize = require('sequelize');
+// const sequelize = require('../util/db');
 
-const Order = sequelize.define('order', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  }
-});
+// const Order = sequelize.define('order', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   }
+// });
 
-module.exports = Order;
+// module.exports = Order;
